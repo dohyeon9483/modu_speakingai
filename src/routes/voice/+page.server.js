81b248c86d5ec49ca84aacb5235b8fb62cadeed8
dhkim <dh4934@naver.com>
2025-11-12 17:57:41 +0900

    login

--- src/routes/voice/+page.server.js
+++ src/routes/voice/+page.server.js
@@ -2,12 +2,13 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
-	// 로그인하지 않은 사용자는 로그인 페이지로 리다이렉트
 	if (!locals.user) {
 		throw redirect(307, '/login');
 	}
 
-	// 로그인된 사용자는 음성 대화 페이지로 이동
-	throw redirect(302, '/voice');
+	return {
+		user: locals.user
+	};
 }
+
 
